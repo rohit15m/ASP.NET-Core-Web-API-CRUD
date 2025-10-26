73a324b79ea6621d3b615f60382b950a15206d92
rohit15m <rohitmaddheshiya119@gmail.com>
2025-10-26 19:14:02 +0530

    Add project files.

---
+++ AspCorewebApiCRUD/Controllers/StudentApiControlleR.cs
@@ -0,0 +1,80 @@
+﻿using AspCorewebApiCRUD.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspCorewebApiCRUD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentApiControlleR : ControllerBase
+    {
+        private readonly CodefirstDb1Context context;
+
+        public StudentApiControlleR(CodefirstDb1Context context)
+        {
+            this.context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Student>>> GetAllStudents()
+        {
+            var students = await context.Students.ToListAsync();
+            return Ok(students);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Student>> GetStudentByID(int id
+            )
+        {
+            var student = await context.Students.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return student;
+        }
+
+        [HttpPost]
+   
+        public async Task<ActionResult<Student>> CreateStudent(Student std)
+        {
+            await context.Students.AddAsync(std);
+            await context.SaveChangesAsync();
+            return Ok(std); 
+                }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Student>> UpdateStudent(int id, Student std)
+        {
+            if (id != std.Id)
+            {
+                return BadRequest();
+            }
+            context.Entry(std).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+            return Ok(std);
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Student>> DeleteStudent(int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            {
+                var     std = await context.Students.FindAsync(id);
+                if (std == null)
+                {
+                    return NotFound();
+                } 
+
+                     context.Students.Remove(std);
+                     await context.SaveChangesAsync();
+                     return Ok(std);
+            }
+          
+
+        }
+    }
+}
---
+++ AspCorewebApiCRUD/Models/Officersdatum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AspCorewebApiCRUD.Models;
+
+public partial class Officersdatum
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Position { get; set; } = null!;
+
+    public int Salary { get; set; }
+
+    public string State { get; set; } = null!;
+
+    public string AnyFir { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+}
